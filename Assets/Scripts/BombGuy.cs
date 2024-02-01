c96c6754d996d4946fbe00d3d730928884153f01
wsinp431 <wsinp431@naver.com>
2024-02-01 18:53:53 +0900

    업데이트

--- Assets/Scripts/BombGuy.cs
+++ Assets/Scripts/BombGuy.cs
@@ -7,22 +7,22 @@ public class BombGuy : MonoBehaviour
 {
     enum State { Idle, Run };//애니메이션 상태
     Animator anim;
-    private Coroutine coroutine;//코루틴을 변수로 선언한 이유 - StopCoroutine를 쓰기 위해서
+    //private Coroutine coroutine;//코루틴을 변수로 선언하는 이유 - StopCoroutine를 쓰기 위해서
     [SerializeField] private float moveSpeed = 1.5f;
     [SerializeField] private GameObject flag;
     private int dir = 0;
     private void Awake()
     {
         anim = GetComponent<Animator>();//Animator컴포넌트를 가져오는 방법
-        //coroutine = StartCoroutine(CoMove(() => { Debug.Log("코루틴 종료"); })); //StopCoroutine을 쓰기 전 어떤 코루틴을 멈출 것인지 지정한다.
-        coroutine = StartCoroutine(CoMove());
+        StartCoroutine(CoMove(() => { Debug.Log("코루틴 종료"); })); //StopCoroutine을 쓰기 전 어떤 코루틴을 멈출 것인지 지정한다.
+        //coroutine = StartCoroutine(CoMove());
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))//마우스를 클릭하면
+   /*     if (Input.GetMouseButtonDown(0))//마우스를 클릭하면
         {
             StopCoroutine(coroutine);//코루틴 정지. 코루틴 인스턴스를 매개변수로 받는다
-        }
+        }*/
         if (Input.GetKey(KeyCode.RightArrow))
         {
             dir = 1;
@@ -47,28 +47,28 @@ public class BombGuy : MonoBehaviour
     {
         anim.SetInteger("State", (int)state);
     }
-    //IEnumerator CoMove(Action callback)
-    //{
-    //    while (true)
-    //    {
-    //        transform.Translate(dir * moveSpeed * Time.deltaTime, 0, 0);
-    //        float length = flag.transform.position.x - transform.position.x;//깃발과의 거리
-    //        if (length < 0.5f)//거리가 0.5이하면
-    //            break;//루프를 벗어난다
-    //        yield return null;//다음 '프레임'으로 넘어간다
-    //        callback();
-    //    }
-    //}    
-    IEnumerator CoMove()
+    IEnumerator CoMove(Action callback)
     {
         while (true)
         {
-            Debug.Log(this.dir);
-            transform.Translate(this.dir * this.moveSpeed * Time.deltaTime, 0, 0);
+            transform.Translate(dir * moveSpeed * Time.deltaTime, 0, 0);
             float length = flag.transform.position.x - transform.position.x;//깃발과의 거리
             if (length < 0.5f)//거리가 0.5이하면
                 break;//루프를 벗어난다
             yield return null;//다음 '프레임'으로 넘어간다
+            callback();
         }
     }
+    //IEnumerator CoMove()
+    //{
+    //    while (true)
+    //    {
+    //        Debug.Log(this.dir);
+    //        transform.Translate(this.dir * this.moveSpeed * Time.deltaTime, 0, 0);
+    //        float length = flag.transform.position.x - transform.position.x;//깃발과의 거리
+    //        if (length < 0.5f)//거리가 0.5이하면
+    //            break;//루프를 벗어난다
+    //        yield return null;//다음 '프레임'으로 넘어간다
+    //    }
+    //}
 }
